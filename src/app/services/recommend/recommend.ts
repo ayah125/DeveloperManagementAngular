c4893c2edfcbd3d4b756e5b6eec3a256d7cd8023
Ahmed Taher <90255766+ahmedtaher45@users.noreply.github.com>
2025-08-08 14:33:35 +0300

    update trcommend model

--- src/app/services/recommend/recommend.ts
+++ src/app/services/recommend/recommend.ts
@@ -1,19 +1,30 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AssignDeveloperData } from '../../interfaces/assign-developer-data';
 import { env } from '../../../enviroment/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class Recommend {
-
   constructor(private http: HttpClient) {}
 
   getRecommendations(description: string): Observable<any> {
+    const token = localStorage.getItem('userToken');
+
+    if (!token) {
+      throw new Error('Authentication token not found in localStorage');
+    }
+
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    });
     const body = { description: description };
-    return this.http.post<any>(`${env.apiUrl}api/GenAIModels/Recommend`, body);
+    return this.http.post<any>(`${env.apiUrl}api/GenAIModels/Recommend`, body, {
+      headers,
+    });
   }
 
   AssignDeveloper(assignData: AssignDeveloperData): Observable<any> {
