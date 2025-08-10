775b72cf69218e2c1ddee1c347b7080eac7b15ba
Mostafa-Mohamed2001 <Mostafa2001Mohamed2001@gmail.com>
2025-08-10 19:27:45 +0300

    اللاستاية باذن الله

--- src/app/pages/docs/docs.ts
+++ src/app/pages/docs/docs.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-docs',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './docs.html',
-  styleUrl: './docs.css'
+  styleUrls: ['./docs.css']
 })
 export class Docs {
+  constructor(private router: Router, private route: ActivatedRoute) {}
 
+  ngAfterViewInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        const fragment = this.route.snapshot.fragment;
+        if (fragment) {
+          const element = document.getElementById(fragment);
+          if (element) {
+            element.scrollIntoView({ behavior: 'smooth' });
+          }
+        }
+      }
+    });
+  }
 }
